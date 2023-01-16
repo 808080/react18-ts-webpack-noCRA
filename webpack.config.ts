08bfb178492d908926e9b4983bffbc59eb3b4053
szaderaka <sergey.zaderaka@quadcode.com>
2023-01-16 20:12:31 +0300

    init commit

---
+++ webpack.config.ts
@@ -0,0 +1,45 @@
+import { Configuration } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+const config: Configuration = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    compress: true,
+    open: true,
+    hot: true
+  },
+  watchOptions: {
+    ignored: /node_modules/
+  }
+};
+
+export default config;
